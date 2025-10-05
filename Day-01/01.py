84b7c932ca7bcce1ac67c13db3027d1a9cce64fa
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-10-05 18:14:23 +0545

    Add Day-01: Initial Python script functionality

---
+++ Day-01/01.py
@@ -0,0 +1,14 @@
+print("Hello World")
+'''
+It is multi line comments which is docs String....
+
+'''
+sher = "Lion0" 
+print(sher)
+
+Name = "Harry"
+Age = 12
+print(Name, Age)
+
+dec= 122j
+print(type(dec))
