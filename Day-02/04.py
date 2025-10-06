9479ca5ac2745519f05d5b63fdce67a15e1fc7c4
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-10-06 20:02:21 +0545

    Add Day-02/04: check if user input number is odd or even

---
+++ Day-02/04.py
@@ -0,0 +1,10 @@
+# Print the number is odd or even asking the user to enter a number
+
+num = (input ("Enter a number : "))
+
+if num %2==0:
+    print("the number is even")
+elif num % 2 !=0:
+    print("the number is odd")
+else:
+    print("the number is not a number")
