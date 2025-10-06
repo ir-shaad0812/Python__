d75f74d1c389de5d340e2cb556183122764f14c7
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-10-06 19:59:19 +0545

    Add Day-02/01: if-else practice set 3

---
+++ Day-02/01.py
@@ -0,0 +1,26 @@
+# Conditional Statement if else 
+# a = 13
+# if a < 10:
+#     print("Number is greater than a")
+# else:
+#     print("Not greater A") 
+# print( "a value is not executable" ) 
+
+# # Conditional Statement if -2
+# money = int (input ("Enter the money which provided by mommy: -"))
+
+# if money ==100:
+#     print("You can buy energy drink.. ")
+# else:
+#     print("You cannot buy energy drink,.. ")
+
+
+# Conditional Statement -03 if - elif :
+money = int(input("Enter the money provided by mommy: "))
+
+if money == 100:
+    print("You can buy an energy drink.")
+elif money == 200:
+    print("You can buy tea.")
+else:
+    print("Not enough money for energy drink or tea.")
