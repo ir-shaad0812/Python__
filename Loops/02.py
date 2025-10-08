7b63459d5aa96be4a54a9e80a7c426a02656d17a
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-10-08 22:59:36 +0545

    Add string iteration using range function

---
+++ Loops/02.py
@@ -0,0 +1,12 @@
+  
+
+a = "MSSIR is a great company"
+print(len(a)) 
+for i in range (len(a)):
+    print(a[i])
+
+# Another method to iterate string wihtout using range function
+b = "It is string value with another process"
+print(len(b))
+for i in b:
+    print(i)
