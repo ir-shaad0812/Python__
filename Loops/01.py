00ab5b4c39aa7b5e2aff61633fd8251441dd35aa
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-10-15 20:05:06 +0545

    Add loop to generate multiplication table from user input

--- Loops/01.py
+++ Loops/01.py
@@ -3,6 +3,7 @@
 #     print(i)
     # let's print a  table of 13
 
+# loops for table of any number from user input
 num = int (input("Enter a number to print its table: "))
 for i in range (num, (num*10)+1, num):
     print(i)
